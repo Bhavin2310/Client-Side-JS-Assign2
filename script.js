1f9ac7b047d09afdbed21f5ddf3478fa7e532661
Bhavin2310 <141088055+Bhavin2310@users.noreply.github.com>
2025-03-21 22:45:31 -0400

    select the fruits and extras and it will create a name based on selection

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ class Smoothie {
         this.extras = extras || []; // same for extras
         this.size = size;
     }
+    // name based on what user has selected
+    getSmoothieName() {
+        let name = "";
+        
+        // check fruits 1st
+        if (this.fruits.length > 0) {
+            name = this.fruits[0]; // select the fruit
+            if (this.fruits.length > 1) {
+                name += " Mix"; // add mix if more 1 or more fruits
+            }
+        } else {
+            name = "Plain"; // if not selected, just plain
+        }
+
+        // add the base 
+        name += " " + this.base;
+
+        // protein or else peanut butter
+        if (this.extras.includes("Protein Powder")) {
+            name += " Power";
+        } else if (this.extras.includes("Peanut Butter")) {
+            name += " Nutty";
+        }
+
+        return name;
+    }
